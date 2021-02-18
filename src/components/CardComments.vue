e9428864624521e3f549ff58ec59640d124269d4
EfimKornienko <efim3009@gmail.com>
2021-02-19 00:15:50 +0300

    fix component names

--- src/components/CardComments.vue
+++ src/components/CardComments.vue
@@ -18,7 +18,7 @@
 
 <script>
 export default {
-  name: 'Post',
+  name: 'CardComments',
 
   props: {
     comments: {
--- src/components/CardPost.vue
+++ src/components/CardPost.vue
@@ -23,7 +23,7 @@
 
 <script>
 export default {
-  name: 'Post',
+  name: 'CardPost',
 
   props: {
     post: {
